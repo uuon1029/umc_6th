70cd4427856b012168c14c20646ff1a1e710a398
uuon <uuon1029@mail.ulsan.ac.kr>
2024-07-12 13:13:48 +0900

    temp

---
+++ app/src/main/java/com/example/umc_6th/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.umc_6th
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_6th.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBottomNavigation.setOnItemSelectedListener {
+            item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.writeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, WriteFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.shopFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, ShopFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.configFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, ConfigFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/umc_6th/WriteFragment.kt
@@ -0,0 +1,23 @@
+package com.example.umc_6th
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.umc_6th.databinding.FragmentWriteBinding
+
+class WriteFragment : Fragment() {
+
+    lateinit var binding: FragmentWriteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWriteBinding.inflate(inflater,container,false)
+
+        return binding.root
+    }
+}
