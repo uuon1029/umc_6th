420dca704284b3f78ae53ea392ac47c3c466d42c
eastlaw02 <ng524855@gmail.com>
2024-07-19 03:20:29 +0900

    add searchResultActivity

---
+++ app/src/main/java/com/example/umc_6th/SearchResultActivity.kt
@@ -0,0 +1,16 @@
+package com.example.umc_6th
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_6th.databinding.ActivitySearchResultBinding
+
+class SearchResultActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySearchResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
