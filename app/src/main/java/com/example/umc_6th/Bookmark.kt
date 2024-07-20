59e2425f4b724c17b5d1fbfbbb5c6f5bf28f885b
eastlaw02 <ng524855@gmail.com>
2024-07-21 00:49:05 +0900

    edit bookmark

--- app/src/main/java/com/example/umc_6th/Bookmark.kt
+++ app/src/main/java/com/example/umc_6th/Bookmark.kt
@@ -2,5 +2,6 @@ package com.example.umc_6th
 
 data class Bookmark(
     var title : String? = "",
-    var content : String? = ""
+    var content : String? = "",
+    var ismarked : Boolean = false
 )
