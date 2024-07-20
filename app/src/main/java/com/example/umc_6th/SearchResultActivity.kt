f6b782ee3a8d82bd2a8febf6d08c0746ce298cdc
eastlaw02 <ng524855@gmail.com>
2024-07-20 23:11:44 +0900

    transaction SearchResultActivity

--- app/src/main/java/com/example/umc_6th/SearchResultActivity.kt
+++ app/src/main/java/com/example/umc_6th/SearchResultActivity.kt
@@ -12,5 +12,11 @@ class SearchResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val explainFragment = ExplainFragment()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.search_result_main_frm,explainFragment)
+            .commitAllowingStateLoss()
     }
 }
