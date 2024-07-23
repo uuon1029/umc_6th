825010737a75cee5874790d3406acf169114630f
eastlaw02 <ng524855@gmail.com>
2024-07-23 21:08:13 +0900

    add history dropdown

--- app/src/main/java/com/example/umc_6th/ConfigHistoryFragment.kt
+++ app/src/main/java/com/example/umc_6th/ConfigHistoryFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.SpinnerAdapter
+import androidx.annotation.VisibleForTesting
 import androidx.fragment.app.Fragment
 import com.example.umc_6th.databinding.FragmentConfigHistoryBinding
 
@@ -14,6 +12,7 @@ class ConfigHistoryFragment : Fragment() {
 
     lateinit var binding: FragmentConfigHistoryBinding
     private var isOpened : Boolean = false
+    private var isSelected : Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +21,7 @@ class ConfigHistoryFragment : Fragment() {
     ): View? {
         binding = FragmentConfigHistoryBinding.inflate(inflater,container,false)
 
-        binding.configHistorySwitchImgIv.setOnClickListener {
+        binding.configHistoryOpenBtnIv.setOnClickListener {
             if(isOpened) {
                 binding.configHistorySwitchImgIv.setImageResource(R.drawable.ic_switch_close)
                 binding.configHistorySwitchTv.text = "비공개"
@@ -35,8 +34,47 @@ class ConfigHistoryFragment : Fragment() {
             isOpened = !isOpened
         }
 
+        setupDropdown()
+
         return binding.root
     }
 
+    private fun setupDropdown() {
+        binding.configHistorySelectOptionCl.setOnClickListener {
+            binding.configHistorySelectOptionCl.visibility = View.GONE
+            binding.configHistoryDropdownCl.visibility = View.VISIBLE
+        }
+        binding.configHistorySelectedCl.setOnClickListener {
+            binding.configHistorySelectOptionCl.visibility = View.VISIBLE
+            binding.configHistoryDropdownCl.visibility = View.GONE
+        }
 
+        binding.configHistoryDropdownWrittingCl.setOnClickListener {
+            binding.configHistoryDropdownSelectedTv.text = "내가 쓴 글"
+            binding.configHistorySelectOptionSelectedTv.text = "내가 쓴 글"
+            binding.configHistorySelectOptionCl.visibility = View.VISIBLE
+            binding.configHistoryDropdownCl.visibility = View.GONE
+        }
+
+        binding.configHistoryDropdownCommentCl.setOnClickListener {
+            binding.configHistoryDropdownSelectedTv.text = "댓글 단 글"
+            binding.configHistorySelectOptionSelectedTv.text = "댓글 단 글"
+            binding.configHistorySelectOptionCl.visibility = View.VISIBLE
+            binding.configHistoryDropdownCl.visibility = View.GONE
+        }
+
+        binding.configHistoryDropdownBookmarkCl.setOnClickListener {
+            binding.configHistoryDropdownSelectedTv.text = "즐겨찾기"
+            binding.configHistorySelectOptionSelectedTv.text = "즐겨찾기"
+            binding.configHistorySelectOptionCl.visibility = View.VISIBLE
+            binding.configHistoryDropdownCl.visibility = View.GONE
+        }
+
+        binding.configHistoryDropdownAllCl.setOnClickListener {
+            binding.configHistoryDropdownSelectedTv.text = "전체"
+            binding.configHistorySelectOptionSelectedTv.text = "전체"
+            binding.configHistorySelectOptionCl.visibility = View.VISIBLE
+            binding.configHistoryDropdownCl.visibility = View.GONE
+        }
+    }
 }
