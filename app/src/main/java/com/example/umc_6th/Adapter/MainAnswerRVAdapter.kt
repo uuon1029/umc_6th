a8a87445c889b043f935a1a5ef0d4c3f4245706e
uuon <uuon1029@mail.ulsan.ac.kr>
2024-07-24 23:56:55 +0900

    quest like

--- app/src/main/java/com/example/umc_6th/Adapter/MainAnswerRVAdapter.kt
+++ app/src/main/java/com/example/umc_6th/Adapter/MainAnswerRVAdapter.kt
@@ -2,6 +2,7 @@ package com.example.umc_6th
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,15 @@ import com.example.umc_6th.databinding.ItemQuestMainAnswerBinding
 
 class MainAnswerRVAdapter(private val context: Context) : RecyclerView.Adapter<MainAnswerRVAdapter.Holder>() {
     var itemList = ArrayList<MainAnswer>()
+
+//    interface SetClick {
+//
+//    }
+//    private lateinit var setClick : SetClick
+//    fun initSetClick(itemSetClick : SetClick) {
+//        setClick = itemSetClick
+//    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ItemQuestMainAnswerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -18,11 +28,19 @@ class MainAnswerRVAdapter(private val context: Context) : RecyclerView.Adapter<M
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val item = itemList[position]
         holder.bind(item)
+        holder.binding.itemQuestMainAnwserLikeIv.setOnClickListener {
+            holder.binding.itemQuestMainAnwserLikeIv.visibility = View.GONE
+            holder.binding.itemQuestMainAnwserUnlikeIv.visibility = View.VISIBLE
+        }
+        holder.binding.itemQuestMainAnwserUnlikeIv.setOnClickListener {
+            holder.binding.itemQuestMainAnwserLikeIv.visibility = View.VISIBLE
+            holder.binding.itemQuestMainAnwserUnlikeIv.visibility = View.GONE
+        }
     }
 
     override fun getItemCount(): Int = itemList.size
 
-    class Holder(private val binding: ItemQuestMainAnswerBinding, private val context: Context) : RecyclerView.ViewHolder(binding.root) {
+    class Holder(val binding: ItemQuestMainAnswerBinding, private val context: Context) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: MainAnswer) {
             binding.itemQuestMainAnwserNameTv.text = item.main_answer_name
             binding.itemQuestMainAnwserBodyTv.text = item.main_answer_body
