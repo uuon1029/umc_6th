01b2fee30eb56cc88247970745370e500ecfe542
zerojung <claraa03063@gmail.com>
2024-08-05 21:34:41 +0900

    board all, community, major, hot db

--- app/src/main/java/com/example/umc_6th/Fragment/MoreTotalBoardFragment.kt
+++ app/src/main/java/com/example/umc_6th/Fragment/MoreTotalBoardFragment.kt
@@ -9,14 +9,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.umc_6th.Activity.CommunitySearchActivity
+import com.example.umc_6th.Retrofit.BoardAllListResponse
+import com.example.umc_6th.Retrofit.BoardMajorListResponse
+import com.example.umc_6th.Retrofit.DataClass.Board
+import com.example.umc_6th.Retrofit.RetrofitClient
 import com.example.umc_6th.databinding.FragmentMoreTotalboardBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MoreTotalBoardFragment : Fragment(){
     lateinit var binding: FragmentMoreTotalboardBinding
     private lateinit var adapter : MoreTotalBoardRVAdapter
 
-    var MoreTotalBoardDatas = ArrayList<More>()
+    var MoreTotalBoardDatas = ArrayList<Board>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,10 +39,35 @@ class MoreTotalBoardFragment : Fragment(){
             val i = Intent(activity, CommunitySearchActivity::class.java)
             startActivity(i)
         }
-        initializemoretotalboardlist()
-        initmoretotalboardRecyclerView()
+        //initializemoretotalboardlist()
+        callGetBoardTotal()
 
         return binding.root
+    }
+
+    private fun callGetBoardTotal() {
+
+        RetrofitClient.service.getBoardAll(1).enqueue(object :
+            Callback<BoardAllListResponse> {
+            override fun onFailure(call: Call<BoardAllListResponse>?, t: Throwable?) {
+                Log.e("retrofit", t.toString())
+            }
+
+            override fun onResponse(
+                call: Call<BoardAllListResponse>?,
+                response: Response<BoardAllListResponse>?
+            ) {
+                Log.d("retrofit", response.toString())
+                Log.d("retrofit", response?.code().toString())
+                Log.d("retrofit", response?.message().toString())
+                Log.d("retrofit", response?.body()?.result.toString())
+
+                if (response != null ) {
+                    MoreTotalBoardDatas = response.body()?.result?.boardList!!
+                    initmoretotalboardRecyclerView()
+                }
+            }
+        })
     }
 
     fun initmoretotalboardRecyclerView(){
@@ -43,7 +75,7 @@ class MoreTotalBoardFragment : Fragment(){
         adapter.moreTotalBoardlist = MoreTotalBoardDatas
         Log.d("List",adapter.moreTotalBoardlist.toString())
         adapter.setMyItemClickListener(object : MoreTotalBoardRVAdapter.MyItemClickListener {
-            override fun onItemClick(more: More) {
+            override fun onItemClick(board: Board) {
                 val intent = Intent(activity, QuestActivity::class.java)
                 startActivity(intent)
             }
@@ -53,15 +85,15 @@ class MoreTotalBoardFragment : Fragment(){
         binding.moreTotalboardQuestRv.layoutManager=LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
     }
 
-    fun initializemoretotalboardlist(){
-        with(MoreTotalBoardDatas){
-            add(More("전체 질문 1 제목","전체 질문 1 내용",21,0,1,R.drawable.ic_rectangle_gray_60,"톰" ))
-            add(More("전체 질문 2 제목","전체 질문 2 내용",22,2,3,R.drawable.ic_rectangle_gray_60,"원" ))
-            add(More("전체 질문 3 제목","전체 질문 3 내용",23,4,5,R.drawable.ic_rectangle_gray_60,"퓨리" ))
-            add(More("전체 질문 4 제목","전체 질문 4 내용",24,6,7,R.drawable.ic_rectangle_gray_60,"제로" ))
-            add(More("전체 질문 5 제목","전체 질문 5 내용",25,8,9,R.drawable.ic_rectangle_gray_60,"노아" ))
-            add(More("전체 질문 6 제목","전체 질문 6 내용",26,10,11,R.drawable.ic_rectangle_gray_60,"킴러브" ))
-        }
-    }
+//    fun initializemoretotalboardlist(){
+//        with(MoreTotalBoardDatas){
+//            add(More("전체 질문 1 제목","전체 질문 1 내용",21,0,1,R.drawable.ic_rectangle_gray_60,"톰" ))
+//            add(More("전체 질문 2 제목","전체 질문 2 내용",22,2,3,R.drawable.ic_rectangle_gray_60,"원" ))
+//            add(More("전체 질문 3 제목","전체 질문 3 내용",23,4,5,R.drawable.ic_rectangle_gray_60,"퓨리" ))
+//            add(More("전체 질문 4 제목","전체 질문 4 내용",24,6,7,R.drawable.ic_rectangle_gray_60,"제로" ))
+//            add(More("전체 질문 5 제목","전체 질문 5 내용",25,8,9,R.drawable.ic_rectangle_gray_60,"노아" ))
+//            add(More("전체 질문 6 제목","전체 질문 6 내용",26,10,11,R.drawable.ic_rectangle_gray_60,"킴러브" ))
+//        }
+//    }
 
 }
--- app/src/main/java/com/example/umc_6th/Retrofit/DataClass/Board.kt
+++ app/src/main/java/com/example/umc_6th/Retrofit/DataClass/Board.kt
@@ -3,6 +3,7 @@ package com.example.umc_6th.Retrofit.DataClass
 import com.google.gson.annotations.SerializedName
 
 data class Board(
+    @SerializedName("id") val id: Int,
     @SerializedName("title") val title: String,
     @SerializedName("content") val content: String,
     @SerializedName("pinCount") val pinCount: Int,
