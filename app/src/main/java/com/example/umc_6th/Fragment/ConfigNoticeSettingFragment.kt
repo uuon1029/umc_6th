b5030cd6388073ed5354cfffe7b6c47ae779dae9
uuon <uuon1029@mail.ulsan.ac.kr>
2024-08-06 02:45:46 +0900

    notice setting

--- app/src/main/java/com/example/umc_6th/Fragment/ConfigNoticeSettingFragment.kt
+++ app/src/main/java/com/example/umc_6th/Fragment/ConfigNoticeSettingFragment.kt
@@ -23,6 +23,26 @@ class ConfigNoticeSettingFragment : Fragment() {
                 .replace(R.id.main_frm,ConfigFragment()).commitAllowingStateLoss()
         }
 
+        binding.configNoticeSettingOption1Ib.setOnClickListener{
+            binding.configNoticeSettingSwitch1Ib.isSelected = !binding.configNoticeSettingSwitch1Ib.isSelected
+        }
+
+        binding.configNoticeSettingOption2Ib.setOnClickListener{
+            binding.configNoticeSettingSwitch2Ib.isSelected = !binding.configNoticeSettingSwitch2Ib.isSelected
+        }
+
+        binding.configNoticeSettingOption3Ib.setOnClickListener{
+            binding.configNoticeSettingSwitch3Ib.isSelected = !binding.configNoticeSettingSwitch3Ib.isSelected
+        }
+
+        binding.configNoticeSettingOption4Ib.setOnClickListener{
+            binding.configNoticeSettingSwitch4Ib.isSelected = !binding.configNoticeSettingSwitch4Ib.isSelected
+        }
+
+        binding.configNoticeSettingOption5Ib.setOnClickListener{
+            binding.configNoticeSettingSwitch5Ib.isSelected = !binding.configNoticeSettingSwitch5Ib.isSelected
+        }
+
         return binding.root
     }
 }
