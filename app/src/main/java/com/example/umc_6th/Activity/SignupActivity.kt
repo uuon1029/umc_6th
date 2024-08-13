5cd06e30a5114ff4f019aaa173bf73ea85618595
uuon <uuon1029@mail.ulsan.ac.kr>
2024-08-14 01:22:42 +0900

    signupActivity edit temp

--- app/src/main/java/com/example/umc_6th/Activity/SignupActivity.kt
+++ app/src/main/java/com/example/umc_6th/Activity/SignupActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -45,12 +46,257 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initSetOnClickListener()
-        initFocusET()
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
 
-//        initSpinner()
+        initEtFocus()
 
         signupActivity = this
+    }
+
+    private fun editTextStatus(view: EditText, status: Int) {
+        // status - { 0 : 기본, 1 : 입력 중, 2 : 에러 }
+        when(status) {
+            0 -> view.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_gray40_2)
+            1 -> view.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_black_5)
+            2 -> view.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_error_4)
+        }
+    }
+
+    private fun initEtFocus(){
+        // 이름
+        binding.signupEditNameEt.setOnFocusChangeListener { v, hasFocus ->
+            val view = binding.signupEditNameEt
+            when(hasFocus){
+                true -> {
+                    view.isSelected = false
+                    editTextStatus(view,1)
+                }
+                false -> {
+                    binding.signupNameErrorTv.visibility = View.VISIBLE
+                    binding.signupNameErrorIv.visibility = View.VISIBLE
+                    binding.signupNameErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.gray60))
+
+                    if(view.text.isEmpty()) {
+                        binding.signupNameErrorTv.text = "이름을 입력해 주세요."
+                        binding.signupNameErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupNameErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    } else {
+                        binding.signupNameErrorTv.text = "이름이 확인 되었습니다."
+                        binding.signupNameErrorIv.setImageResource(R.drawable.ic_check)
+                        binding.signupNameErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.gray60))
+
+                        view.isSelected = false
+                    }
+                    when (view.isSelected) {
+                        true -> editTextStatus(view,2)
+                        false -> editTextStatus(view,0)
+                    }
+                }
+            }
+        }
+
+        // 닉네임
+        binding.signupEditNameEt.setOnFocusChangeListener { v, hasFocus ->
+            val view = binding.signupEditNameEt
+            when(hasFocus){
+                true -> {
+                    view.isSelected = false
+                    editTextStatus(view,1)
+                }
+                false -> {
+                    binding.signupNickErrorTv.visibility = View.VISIBLE
+                    binding.signupNickErrorIv.visibility = View.VISIBLE
+
+                    if(view.text.isEmpty()) {
+                        binding.signupNickErrorTv.text = "닉네임을 입력해 주세요."
+                        binding.signupNickErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupNickErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    else {
+                        binding.signupNickErrorTv.text = "닉네임 중복을 확인해 주세요."
+                        binding.signupNickErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupNickErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    when (view.isSelected) {
+                        true -> editTextStatus(view,2)
+                        false -> editTextStatus(view,0)
+                    }
+                }
+            }
+        }
+
+        // 아이디
+        binding.signupEditIdEt.setOnFocusChangeListener { v, hasFocus ->
+            val view = binding.signupEditIdEt
+            when(hasFocus){
+                true -> {
+                    view.isSelected = false
+                    editTextStatus(view,1)
+                }
+                false -> {
+                    binding.signupIdErrorTv.visibility = View.VISIBLE
+                    binding.signupIdErrorIv.visibility = View.VISIBLE
+
+                    if(view.text.isEmpty()) {
+                        binding.signupIdErrorTv.text = "아이디를 입력해 주세요."
+                        binding.signupIdErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupIdErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    else {
+                        binding.signupIdErrorTv.text = "아이디 중복을 확인해 주세요."
+                        binding.signupIdErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupIdErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    when (view.isSelected) {
+                        true -> editTextStatus(view,2)
+                        false -> editTextStatus(view,0)
+                    }
+                }
+            }
+        }
+
+        // 비밀번호
+        binding.signupEditPwEt.setOnFocusChangeListener { v, hasFocus ->
+            val view = binding.signupEditPwEt
+            when(hasFocus){
+                true -> {
+                    view.isSelected = false
+                    editTextStatus(view,1)
+                }
+                false -> {
+                    binding.signupPwErrorTv.visibility = View.VISIBLE
+                    binding.signupPwErrorIv.visibility = View.VISIBLE
+
+                    if(view.text.isEmpty()) {
+                        binding.signupPwErrorTv.text = "비밀번호를 입력해 주세요."
+                        binding.signupPwErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupPwErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    else if(view.text.length < 8 || view.text.length > 20){
+                        binding.signupPwErrorTv.text = "다시 입력해 주세요."
+                        binding.signupPwErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupPwErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    } else {
+                        binding.signupPwErrorTv.text = "사용 가능한 비밀번호입니다."
+                        binding.signupPwErrorIv.setImageResource(R.drawable.ic_check)
+                        binding.signupPwErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = false
+                    }
+                    when (view.isSelected) {
+                        true -> editTextStatus(view,2)
+                        false -> editTextStatus(view,0)
+                    }
+                }
+            }
+        }
+
+        // 비밀번호 체크
+        binding.signupEditCheckPwEt.setOnFocusChangeListener { v, hasFocus ->
+            val view = binding.signupEditCheckPwEt
+            when(hasFocus){
+                true -> {
+                    view.isSelected = false
+                    editTextStatus(view,1)
+                }
+                false -> {
+                    binding.signupCheckPwErrorTv.visibility = View.VISIBLE
+                    binding.signupCheckPwErrorIv.visibility = View.VISIBLE
+
+                    if(view.text.isEmpty()) {
+                        binding.signupCheckPwErrorTv.text = "닉네임을 입력해 주세요."
+                        binding.signupCheckPwErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupCheckPwErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    else {
+                        binding.signupCheckPwErrorTv.text = "닉네임 중복을 확인해 주세요."
+                        binding.signupCheckPwErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupCheckPwErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    when (view.isSelected) {
+                        true -> editTextStatus(view,2)
+                        false -> editTextStatus(view,0)
+                    }
+                }
+            }
+        }
+
+        // 휴대폰 번호
+//        binding.signupEditPhoneEt.setOnFocusChangeListener { v, hasFocus ->
+//            val view = binding.signupEditPhoneEt
+//            when(hasFocus){
+//                true -> {
+//                    view.isSelected = false
+//                    editTextStatus(view,1)
+//                }
+//                false -> {
+//                    binding.signupPhoneErrorTv.visibility = View.VISIBLE
+//                    binding.signupPhoneErrorTv2.visibility = View.VISIBLE
+//
+//                    if(view.text.isEmpty()) {
+//                        binding.signupPhoneErrorTv.text = "남은 시간:"
+//                        binding.signupPhoneErrorTv2.text = ""
+//                        view.isSelected = true
+//                    }
+//                    when (view.isSelected) {
+//                        true -> editTextStatus(view,2)
+//                        false -> editTextStatus(view,0)
+//                    }
+//                }
+//            }
+//        }
+
+        // 인증번호
+        binding.signupEditAuthEt.setOnFocusChangeListener { v, hasFocus ->
+            val view = binding.signupEditAuthEt
+            when(hasFocus){
+                true -> {
+                    view.isSelected = false
+                    editTextStatus(view,1)
+                }
+                false -> {
+                    binding.signupAuthErrorTv.visibility = View.VISIBLE
+                    binding.signupAuthErrorIv.visibility = View.VISIBLE
+
+                    if(view.text.isEmpty()) {
+                        binding.signupAuthErrorTv.text = "닉네임을 입력해 주세요."
+                        binding.signupAuthErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupAuthErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    else {
+                        binding.signupAuthErrorTv.text = "닉네임 중복을 확인해 주세요."
+                        binding.signupAuthErrorIv.setImageResource(R.drawable.ic_check_error)
+                        binding.signupAuthErrorTv.setTextColor(ContextCompat.getColor(this@SignupActivity,R.color.error_color))
+
+                        view.isSelected = true
+                    }
+                    when (view.isSelected) {
+                        true -> editTextStatus(view,2)
+                        false -> editTextStatus(view,0)
+                    }
+                }
+            }
+        }
     }
 
 
@@ -70,13 +316,7 @@ class SignupActivity : AppCompatActivity() {
 
                     if (response.body() != null){
                         if (!response.body()!!.result){
-                            binding.signupEditNickEt.isSelected = false
-                            initErrorView(binding.signupEditNickEt)
-                            Log.d("retrofit/SignUp_nick-dup", response.body()!!.result.toString())
-                        }
-                        else{
-                            binding.signupEditNickEt.isSelected = true
-                            initErrorView(binding.signupEditNickEt)
+
                         }
                     }
                 }
@@ -97,13 +337,7 @@ class SignupActivity : AppCompatActivity() {
 
                     if (response.body() != null){
                         if (!response.body()!!.result){
-                            binding.signupEditIdEt.isSelected = false
-                            initErrorView(binding.signupEditIdEt)
-                            Log.d("retrofit/SignUp_id-dup", response.body()!!.result.toString())
-                        }
-                        else{
-                            binding.signupEditIdEt.isSelected = true
-                            initErrorView(binding.signupEditIdEt)
+
                         }
                     }
                 }
@@ -194,73 +428,6 @@ class SignupActivity : AppCompatActivity() {
                     }
                 })
             }
-        }
-    }
-
-    private fun initFocusET() {
-        focusView(binding.signupEditNameEt)
-        focusView(binding.signupEditNickEt)
-        focusView(binding.signupEditIdEt)
-        binding.signupEditPwEt.onFocusChangeListener = object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                if(hasFocus) {
-                    binding.signupEditPwEt.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_black_5)
-                }else {
-                    binding.signupEditPwEt.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_gray40_2)
-                }
-                checkPw()
-            }
-        }
-        binding.signupEditCheckPwEt.onFocusChangeListener = object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                if(hasFocus) {
-                    binding.signupEditCheckPwEt.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_black_5)
-                }else {
-                    binding.signupEditCheckPwEt.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_gray40_2)
-                }
-                checkPw()
-            }
-        }
-        focusView(binding.signupEditPhoneEt)
-        focusView(binding.signupEditAuthEt)
-    }
-
-    private fun checkPw() {
-        if((binding.signupEditCheckPwEt.text.toString() == binding.signupEditPwEt.text.toString())
-            &&(binding.signupEditCheckPwEt.text.length in 7..20)){
-            binding.signupEditCheckPwEt.isSelected = false
-        } else {
-            binding.signupEditCheckPwEt.isSelected = true
-        }
-        if(binding.signupEditCheckPwEt.isSelected){
-            binding.signupEditCheckPwEt.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_error_4)
-        }else{
-            binding.signupEditCheckPwEt.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_gray40_2)
-        }
-    }
-
-    private fun focusView(View:TextView) {
-        View.onFocusChangeListener = object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                if(View.text == "") {
-                    View.isSelected = true
-                }
-
-                if(View.isSelected){
-                    View.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_error_4)
-                }else if(hasFocus) {
-                    View.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_black_5)
-                }else {
-                    View.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_gray40_2)
-                }
-            }
-        }
-    }
-
-    private fun initErrorView(View:TextView) {
-        when(View.isSelected){
-            true -> View.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_error_4)
-            false -> View.setBackgroundResource(R.drawable.bg_rectangle_gray20_radius_20_stroke_gray40_2)
         }
     }
 }
