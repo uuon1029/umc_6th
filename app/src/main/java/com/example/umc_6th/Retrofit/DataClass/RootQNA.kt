1179fe64db54c5d02ea64b7c3a49814f35efcb29
uuon <uuon1029@mail.ulsan.ac.kr>
2024-08-17 20:55:40 +0900

    merge

--- app/src/main/java/com/example/umc_6th/Retrofit/DataClass/RootQNA.kt
+++ app/src/main/java/com/example/umc_6th/Retrofit/DataClass/RootQNA.kt
@@ -2,10 +2,10 @@ package com.example.umc_6th.Retrofit.DataClass
 
 import com.google.gson.annotations.SerializedName
 
-data class RootQNA(
-    @SerializedName("id") val id: Int,
-    @SerializedName("status") val status: String,
-    @SerializedName("title") val title: String,
-    @SerializedName("content") val content: String,
-    @SerializedName("createdAt") val createdAt: String
+data class RootQnA(
+    @SerializedName("id")val id : Int,
+    @SerializedName("statue")val status : String,
+    @SerializedName("title")val title : String,
+    @SerializedName("content")val content : String,
+    @SerializedName("createdAt")val createdAt : String
 )
--- app/src/main/java/com/example/umc_6th/Retrofit/Response/RootQNAListResponse.kt
+++ app/src/main/java/com/example/umc_6th/Retrofit/Response/RootQNAListResponse.kt
@@ -1,20 +1,20 @@
 package com.example.umc_6th.Retrofit.Response
 
-import com.example.umc_6th.Retrofit.DataClass.RootQNA
+import com.example.umc_6th.Retrofit.DataClass.RootQnA
 import com.google.gson.annotations.SerializedName
 
-data class RootQNAListResponse(
-    @SerializedName("isSuccess") val isSuccess: Boolean,
-    @SerializedName("code") val code: String,
-    @SerializedName("message") val message: String,
-    @SerializedName("result") val result: Result
+data class RootQnAListResponse(
+    @SerializedName("isSuccess")val isSuccess : Boolean,
+    @SerializedName("code")val code : String,
+    @SerializedName("message")val message : String,
+    @SerializedName("result")val result : Result
 ) {
     data class Result(
-        @SerializedName("rootQNAList") val rootQNAList: ArrayList<RootQNA>,
-        @SerializedName("listSize") val listSize: Int,
-        @SerializedName("totalPage") val totalPage: Int,
-        @SerializedName("totalElements") val totalElements: Int,
-        @SerializedName("isFirst") val isFirst: Boolean,
-        @SerializedName("isLast") val isLast: Boolean
+        @SerializedName("rootQNAList")val rootQNAList : ArrayList<RootQnA>,
+        @SerializedName("listSize")val listSize : Int,
+        @SerializedName("totalPage")val totalPage : Int,
+        @SerializedName("totalElements")val totalElements : Int,
+        @SerializedName("isFirst")val isFirst : Boolean,
+        @SerializedName("isLast")val isLast : Boolean,
     )
 }
