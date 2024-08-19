0ee11923cd9d6c5e7d980faebdd32bba9ba54ad7
uuon <uuon1029@mail.ulsan.ac.kr>
2024-08-19 23:45:01 +0900

    Bookmark dataclass edit

--- app/src/main/java/com/example/umc_6th/Retrofit/DataClass/Bookmark.kt
+++ app/src/main/java/com/example/umc_6th/Retrofit/DataClass/Bookmark.kt
@@ -3,7 +3,10 @@ package com.example.umc_6th.Retrofit.DataClass
 import com.google.gson.annotations.SerializedName
 
 data class Bookmark(
-    @SerializedName("id") val id: Int,
+    @SerializedName("exampleId") val exampleId: Int,
+    @SerializedName("answerId") val answerId: Int,
+    @SerializedName("favoriteId") val favoriteId: Int,
+
     @SerializedName("problem") val problem: String,
     @SerializedName("answer") val answer: String,
     @SerializedName("tag") val tag: String
